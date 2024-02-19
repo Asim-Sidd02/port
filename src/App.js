39a67f68c37807870990ef3870c2b9f3bba670ba
shelooks16 <asimsiddiqui8181@gmail.com>
2024-02-19 20:22:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@
 import './App.css';
 import Header from  './components/header/Header'
 import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
+import Contact from './components/contact/Contact';
+
 
 const App = () => {
   return (
@@ -9,6 +15,12 @@ const App = () => {
     <Header />
     <main className='main'>
       <Home />
+      <About />
+      <Skills />
+      <Services />
+      <Qualification />
+      <Contact />
+    
     </main>
     
     </>
--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -59,7 +59,7 @@ const Data = () => {
         <h3 className="home__subtitle">Front End Developer</h3>
         <p className="home__description">I'm creative designer based in New York, and I'm very passionate and dedicated to my work.
         </p>
-        <a href="" className="button button--flex">
+        <a href="#contact" className="button button--flex">
             Say Hello
 
             <svg
